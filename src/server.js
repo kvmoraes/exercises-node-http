8173c722f356f8922047f0d69d64b4ce932f349f
Karine <karinevitoria.marinho@gmail.com>
2022-07-10 10:59:53 -0300

    show pages

--- src/server.js
+++ src/server.js
@@ -8,9 +8,49 @@
 
   OBS: Deve ser utilizado apenas os módulos nativos do NODE (http, path, fs, etc), nada de instalar outras libs ( ˘︹˘ )
 */
+const http = require ('http');
+const path = require ('path');
+const fs = require ('fs');
 
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
+const port = 8000;
+const host = 'localhost';
 
+function readFile(file) {
+  const filePath = path.join(__dirname, 'pages', `${file}`);
 
+  return fs.readFileSync(filePath, () => {
+    if (err) throw err;
+  })
+}
+
+http
+.createServer((req, res) => {
+  res.setHeader('content-type', 'text/html');
+
+  function createPage(path) {
+    if (path === '/' || path === '/home') {
+      res.writeHead(200);
+      return readFile('index.html');
+    } else if (req.url === '/about') {
+      res.writeHead(200);
+      return readFile('about.html');
+    } else {
+      res.writeHead(404);
+      return readFile('404.html');
+    }
+  }
+  
+  res.end(createPage(req.url));
+})
+.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+})
+.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.log('Adress in use, retrying...');
+    setTimeout(() => {
+      server.close();
+      server.listen(port, host);
+    }, 1000);
+  }
+})
